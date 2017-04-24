fe9228be0de724c8622badbf0fe95966e52cfb2a
Erik <erik.ra@gmail.com>
2017-04-24 03:29:07 +0200

    Add project files.

---
+++ Modulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> nbrs = new HashSet<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                nbrs.Add((input % 42));                
+            }
+            Console.WriteLine(nbrs.Count);
+
+        }
+    }
+}
